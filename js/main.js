d45747122ec75fdfa6c1a0f62cd8b2dbfb687e6e
Abdelghani Adel <abdelghani.private@yahoo.com>
2021-11-01 14:23:09 +0200

    Structured Destination Page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(".nav-btn").click(function () {
   $(".navbar-ul-wrapper").toggleClass("clicked");
 });
+
+let planetPic = $(".planet-pic");
+let planetName = $(".planet-name");
+let planetText = $(".planet-description");
+let planetDestance = $(".destance");
+let planetTravel = $(".travel-time");
+
+$(".planet").click(function (event) {
+  let current = event.currentTarget;
+  let num = current.dataset.num;
+  $.getJSON("../data.json", function (data) {
+    planetPic.attr("src", data["destinations"][num]["images"]["png"]);
+    planetName.text(data["destinations"][num]["name"]);
+    planetText.text(data["destinations"][num]["description"]);
+    planetDestance.text(data["destinations"][num]["distance"]);
+    planetTravel.text(data["destinations"][num]["travel"]);
+  });
+  $(".planet").removeClass("active");
+  $(current).addClass("active");
+});

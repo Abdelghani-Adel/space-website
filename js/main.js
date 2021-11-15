dd4e16ee3cc4698e1784c61330459f59fd2722bf
Abdelghani Adel <abdelghani.private@yahoo.com>
2021-11-15 16:14:47 +0200

    Used JSON to update the ui

--- js/main.js
+++ js/main.js
@@ -17,75 +17,36 @@ navToggle.click(() => {
 const tabList = $('[role="tablist"]'); //Select buttons wrapper
 const tabs = $('[role="tab"]'); // Select buttons
 
-// tabList.addEventListener("keydown", changeTabFocus);
-tabList.keydown(changeTabFocus);
-
-// tabs.each(function () {
-//   // tab.addEventListener("click", changeTabPanel);
-//   $(this).click(changeTabPanel);
-// });
-
 tabs.each((index, element) => {
   $(element).click(changeTabPanel);
 });
 
-let tabFocus = 0;
-function changeTabFocus(e) {
-  const keydownLeft = 37;
-  const keydownRight = 39;
-
-  if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
-    tabs[tabFocus].setAttribute("tabindex", -1);
-
-    if (e.keyCode === keydownRight) {
-      tabFocus++;
-      if (tabFocus >= tabs.length) {
-        tabFocus = 0;
-      }
-    }
-
-    if (e.keyCode === keydownLeft) {
-      tabFocus--;
-      if (tabFocus < 0) {
-        tabFocus = tabs.length - 1;
-      }
-    }
-  }
-
-  tabs[tabFocus].setAttribute("tabindex", 0);
-  tabs[tabFocus].focus();
-}
-
 function changeTabPanel(e) {
-  // Variables
-  const targetTab = $(e.target); // Selecting the button
-  const targetPanel = targetTab.attr("aria-controls"); // Selecting the article link
-  const targetImage = targetTab.attr("data-image"); // Selecting the image link
-  const tabContainer = targetTab.parent(); // Selecting buttons container
-  const mainContainer = tabContainer.parent(); // Selecting the wrapper of (buttons - images - articles)
+  const targetTab = $(e.currentTarget);
+  const tabContainer = targetTab.parent();
+  const num = targetTab.attr("data-num");
 
-  // Change the status of the other active button
-  // Select the current "selected" and make it false
+  const planetImg = $("#planet-img");
+  const planetName = $("#planet-name");
+  const planetInfo = $("#planet-info");
+  const planetDist = $("#planet-dist");
+  const planetTravel = $("#planet-travel");
+
+  // Change the active button
   $(tabContainer).children('[aria-selected="true"]').attr("aria-selected", false);
-
-  // Change the status of the current clicked button to be selected true
   targetTab.attr("aria-selected", true);
 
-  // Hide all articles
-  hideContent(mainContainer, '[role="tabpanel"]');
-  // Show only the current target article
-  showContent(mainContainer, `#${targetPanel}`);
-
-  // Hide all images
-  hideContent(mainContainer, "picture");
-  // Show only the current target image
-  showContent(mainContainer, `#${targetImage}`);
-}
-
-function hideContent(parent, content) {
-  $(parent).children(content).attr("hidden", true);
-}
-
-function showContent(parent, content) {
-  $(parent).children(content).removeAttr("hidden");
+  // Changing the content
+  $.getJSON("../js/data.json", function (data) {
+    // Change the image
+    planetImg.attr("src", data["destinations"][num]["images"]["png"]);
+    // Change the name
+    planetName.text(data["destinations"][num]["name"]);
+    // Change the info
+    planetInfo.text(data["destinations"][num]["description"]);
+    // Change the distance
+    planetDist.text(data["destinations"][num]["distance"]);
+    // change the travel time
+    planetTravel.text(data["destinations"][num]["travel"]);
+  });
 }
